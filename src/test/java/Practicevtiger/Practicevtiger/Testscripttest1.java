7e1a88d6129b487f5e20abaf664cd0af96129a9a
naziyatarnum <naziyatarnum@gmail.com>
2021-09-03 10:29:41 +0530

    push

---
+++ src/test/java/Practicevtiger/Practicevtiger/Testscripttest1.java
@@ -0,0 +1,17 @@
+package Practicevtiger.Practicevtiger;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Testscripttest1 {
+
+	public static void main(String[] args) {
+		
+
+System.out.println("google");
+
+	}
+
+}
